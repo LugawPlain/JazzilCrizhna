68b3a52986a8b13d945bfcb9ddb46f6c65a9f168
LugawPlain <troysarinas22@gmail.com>
2025-04-15 23:15:39 +0800

    optimized for build

--- scripts/update-images-data.js
+++ scripts/update-images-data.js
@@ -1,5 +1,10 @@
-const fs = require("fs");
-const path = require("path");
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Get the directory name of the current module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Configuration
 const categoriesDir = path.join(process.cwd(), "public", "categories");
