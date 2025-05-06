3c27d0c9a4b680a78970f6f26dc6c8b1cd621415
LugawPlain <troysarinas22@gmail.com>
2025-05-07 01:13:25 +0800

    fix social link missing

--- src/components/SocialLinksButton.tsx
+++ src/components/SocialLinksButton.tsx
@@ -59,32 +59,6 @@ const SocialLinksButton = () => {
     {
       icon: (
         <>
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M16 4H8C5.79086 4 4 5.79086 4 8V16C4 18.2091 5.79086 20 8 20H16C18.2091 20 20 18.2091 20 16V8C20 5.79086 18.2091 4 16 4Z"
-          />
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M12 15C13.6569 15 15 13.6569 15 12C15 10.3431 13.6569 9 12 9C10.3431 9 9 10.3431 9 12C9 13.6569 10.3431 15 12 15Z"
-          />
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M16.5 7.5V7.501"
-          />
-        </>
-      ),
-      href: socialLinks.instagram,
-      label: "Instagram",
-    },
-    {
-      icon: (
-        <>
           <TiktokIcon></TiktokIcon>
         </>
       ),
