5d5dc281b624f6fd030b64b8da254eeecc4a02be
LugawPlain <troysarinas22@gmail.com>
2025-05-08 00:29:18 +0800

    fix capitalizaition

--- src/lib/fonts/heebo.ts
+++ src/lib/fonts/heebo.ts
@@ -1,7 +1,7 @@
 import localFont from "next/font/local";
 
 export const heebo = localFont({
-  src: "../../../public/fonts/heebo/Heebo-VariableFont_wght.ttf",
+  src: "../../../public/fonts/Heebo/Heebo-VariableFont_wght.ttf",
   display: "swap",
   variable: "--font-heebo",
 });
--- src/lib/fonts/montserrat.ts
+++ src/lib/fonts/montserrat.ts
@@ -1,7 +1,7 @@
 import localFont from "next/font/local";
 
 export const montserrat = localFont({
-  src: "../../../public/fonts/montserrat/Montserrat-VariableFont_wght.ttf",
+  src: "../../../public/fonts/Montserrat/Montserrat-VariableFont_wght.ttf",
   display: "swap",
   variable: "--font-montserrat",
   weight: "100 900",
