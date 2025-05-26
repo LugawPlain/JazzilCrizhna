0d92c9ec12669bd3e7f3b429f7a8ee1b0e8e7833
LugawPlain <troysarinas22@gmail.com>
2025-05-26 19:40:27 +0800

    about about page story

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -50,28 +50,35 @@ export default function AboutPage() {
             <h2 className="text-2xl sm:text-4xl text-center lg:text-left font-semibold mb-3 sm:mb-4 text-white">
               My Story
             </h2>
-            <p className="text-sm sm:text-base text-gray-300 leading-relaxed">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur.
-            </p>
-            <p className="text-sm sm:text-base text-gray-300 leading-relaxed">
-              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-              officia deserunt mollit anim id est laborum. Sed ut perspiciatis
-              unde omnis iste natus error sit voluptatem accusantium doloremque
-              laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
-              veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-            </p>
-            <p className="text-sm sm:text-base text-gray-300 leading-relaxed">
-              Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
-              aut fugit, sed quia consequuntur magni dolores eos qui ratione
-              voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem
-              ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia
-              non numquam eius modi tempora incidunt ut labore et dolore magnam
-              aliquam quaerat voluptatem.
+            <p className="text-sm sm:text-base text-gray-300 leading-relaxed antialiased tracking-wide">
+              Hi there! ☺️ I'm
+              <span className="font-bold text-lg text-amber-500">
+                {" "}
+                Jazzil Sarinas{" "}
+              </span>{" "}
+              (Yorticia) a model, an influencer, and a brand ambassador based on
+              Cavite.
+              <br />
+              My journey ✈️ began with me participating on sagala festivals 🎉🎏
+              and mall pageants 🏢👠, those early moments sparked a love for
+              self expression confidence and grace 💗💖. What started as a
+              heartfelt passion has blossomed into my purpose.
+              <br /> Over the years, I’ve been so grateful for opportunities
+              like being a basketball league muse 🏀, which gave me a platform
+              not just to be seen, but to inspire, uplift, and share the story
+              and energy ⚡🥰 I bring. Every runway walk, every photoshoot, and
+              every collaboration has sculpted the person I am today, someone
+              who leads with genuine authenticity and a whole lot of heart 💞.
+              <br />
+              Now, I thrive on collaborating with brands, photographers 📸, and
+              fellow creatives 🎨 to craft content that’s more than just
+              stylish, it’s meaningful. Whether it's bringing fashion 👘👗 to
+              life, capturing a lifestyle moment, or energizing a live event, I
+              pour my passion into every detail, always aiming to spark
+              inspiration and leave a positive, lasting impression 💖💖.
+              <br />
+              Thanks for stopping by! 😁 I’m excited by the possibility of
+              connecting and creating something truly beautiful together 💖💖
             </p>
           </motion.div>
         </div>
